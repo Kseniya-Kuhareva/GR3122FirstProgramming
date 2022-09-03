d4d11d984ee8bb9edcdeca22338466d7387fc57e
Kseniya Kuhareva <galkina.k96@mail.ru>
2022-09-04 01:30:57 +0300

    add homework

---
+++ SEM1TASK2/Program.cs
@@ -0,0 +1,24 @@
+﻿//-------------------------------------------------------
+//# 2 Напишите программу, которая на вход принимает два числа и выдаёт, какое число
+//большее, а какое меньшее.
+//-------------------------------------------------------
+
+string? inputLineA = Console.ReadLine();                 //вводим два числа
+string? inputLineB = Console.ReadLine();
+
+if (inputLineA != null && inputLineB != null)            //проверяем на null
+{
+    int inputNumberA = int.Parse(inputLineA);            //делаем числа вещественными
+    int inputNumberB = int.Parse(inputLineB);
+
+    if (inputNumberA > inputNumberB)                     // условие (если неравенство верно)
+    {
+        Console.Write("max = ");                         //в результат выводим это число
+        Console.Write(inputLineA);
+    }
+    else                                                 //а иначе
+    {
+        Console.Write("max = ");                        // в результат выводим это число
+        Console.Write(inputLineB);
+    }
+}                                                       //ценок :)
---
+++ SEM1TASK4/Program.cs
@@ -0,0 +1,38 @@
+﻿// # 4 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+string? inputLineA = Console.ReadLine();                 //вводим три числа
+string? inputLineB = Console.ReadLine();
+string? inputLineC = Console.ReadLine();
+
+if (inputLineA != null && inputLineB != null && inputLineC != null)       //проверяем на null
+
+{
+    int inputNumberA = int.Parse(inputLineA);                //делаем числа вещественными
+    int inputNumberB = int.Parse(inputLineB);
+    int inputNumberС = int.Parse(inputLineC);
+
+    if (inputNumberA > inputNumberB)                         //условие (если неравенство верное)
+        if (inputNumberA > inputNumberС)                      
+        {
+            Console.Write("max = ");                         //выводим число
+            Console.Write(inputLineA);
+        }
+        else                                                 //иначе
+        {
+            Console.Write("max = ");                        //выводим число
+            Console.Write(inputLineC);
+        }
+    else                                             //иначе
+    {
+        if (inputNumberB > inputNumberС)                         //если неравенство верное
+        {
+            Console.Write("max = ");                         //выводим чсило
+            Console.Write(inputLineB);
+        }
+        else                                                     //иначе
+        {
+            Console.Write("max = ");                        //выводим число
+            Console.Write(inputLineC);
+        }
+    }
+}
