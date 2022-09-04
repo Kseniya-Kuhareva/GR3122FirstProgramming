9b8601000b89c11e3ce5d86de59d5bbdeb4a9d1e
Kseniya Kuhareva <galkina.k96@mail.ru>
2022-09-04 19:52:51 +0300

    add homework 2

---
+++ SEM2TASK10/Program.cs
@@ -0,0 +1,15 @@
+﻿// # 10 Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите число:");                                     //пользователь вводит число
+string inputLine = Console.ReadLine() ?? "";                             //консоль считывает число
+if (inputLine.Length == 3)                                               //условие (если число трехзначное)
+{
+    int inputNumber = int.Parse(inputLine);                              //делаем числа вещественными
+    char[] charArray = inputNumber.ToString().ToCharArray();             //делаем из числа массив
+    Console.WriteLine(charArray[1]);                                     //консоль выдает 2 цифру числа
+}
+else                                                                     //иначе
+{
+    Console.WriteLine("Число не является трехзначным");                  //консоль выдает "текст"
+}
---
+++ SEM2TASK13/Program.cs
@@ -0,0 +1,15 @@
+﻿// # 13 Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//что третьей цифры нет.
+
+Console.WriteLine("Введите число:");                                     //пользователь вводит число
+string inputLine = Console.ReadLine() ?? "";                             //консоль считывает число
+if (inputLine.Length >= 3)                                               //условие (если число трехзначное или больше)
+{
+    int inputNumber = int.Parse(inputLine);                              //делаем числа вещественными
+    char[] charArray = inputNumber.ToString().ToCharArray();             //делаем из числа массив
+    Console.WriteLine(charArray[2]);                                     //консоль выдает 3 цифру числа
+}
+else                                                                     //иначе
+{
+    Console.WriteLine("В числе меньше трех цифр");                       //консоль выдает "текст"
+}
