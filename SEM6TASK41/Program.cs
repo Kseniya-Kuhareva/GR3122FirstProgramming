16a5e521096e55a092208f15d3ff1fa2f643b5ee
Kseniya Kuhareva <galkina.k96@mail.ru>
2022-09-18 14:14:24 +0300

    add sem 6

---
+++ SEM6TASK41/Program.cs
@@ -0,0 +1,30 @@
+﻿// #41 Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//Создаем из введенных чисел массив
+int[] GetArray(int arrLength)
+{
+    int[] arr = new int[arrLength];
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        arr[i] = int.Parse(Console.ReadLine() ?? "0");
+    }
+    return arr;
+}
+
+//Считаем количество чисел больше нуля
+int CountPosNums(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите количество чисел: ");
+int arr = int.Parse(Console.ReadLine() ?? "0");
+int[] newArray = GetArray(arr);
+Console.WriteLine("Положительных чисел: " + CountPosNums(newArray));
---
+++ SEM6TASK43/Program.cs
@@ -0,0 +1,37 @@
+﻿// 43 Напишите программу, которая найдёт точку пересечения двух прямых, заданных
+//уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+
+void Point(double b1, double b2, double k1, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = (k1 * b2 - k2 * b1) / (k1 - k2);
+    if (k1 == k2)
+    {
+        if (b1 != b2)
+        {
+            Console.WriteLine("Две прямые параллельны");
+        }
+        else
+        {
+            Console.WriteLine("Две прямые совпадают");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Точка пересечения будет иметь координаты ({x};{y})");
+    }
+}
+Console.WriteLine("Введите переменную b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите переменную k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите переменную b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите переменную k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+
+Point(b1, b2, k1, k2);
+
+
